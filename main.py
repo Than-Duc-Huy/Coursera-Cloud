f76cf29ac98697ac1c56d50e3061b67f4b2dcdd4
td_huy <than_duc_huy@outlook.com>
2023-10-20 13:04:07 +0000

    changes

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello():
     """Return HTTP greeting"""
     print("I am inside hello world")
-    return "Hello World"
+    return "Hello World! There are changes"
 
 @app.route('/echo/<name>')
 def echo(name):
